59ad36e521ac1afe638fadfab6b44e2d71e5e28c
GordonsBeard <thisisgordonsemail@gmail.com>
2015-02-18 19:54:11 -0800

    Fixed an issue related to new users Apparently the order of the openid urls in urls.py matters!

--- elo/urls.py
+++ elo/urls.py
@@ -7,10 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$',                      'ladder.views.index'),
     url(r'^l/',                     include('ladder.urls', namespace = "ladder")),
-
     url(r'^admin/',                 include(admin.site.urls)),
-    url(r'^openid/',                include('django_openid_auth.urls')),
     url(r'^openid/complete/$',      'elo.views.login'),
+    url(r'^openid/',                include('django_openid_auth.urls')),
     url(r'^logout/$',               'elo.views.logout_view'),
 )
 
