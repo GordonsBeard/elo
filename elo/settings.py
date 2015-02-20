15d1d3520e59c51f3f1d4d101973bda3487803dc
David Reeve <nerdboy6@gmail.com>
2015-02-20 15:21:30 -0500

    Oops I committed my settings
    You will have to add usercontrol to your list of installed modules

--- elo/settings.py
+++ elo/settings.py
@@ -94,7 +94,7 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = '/home/gordon/public/laddr.org/elo/static/'
 
 # Steam API key
-STEAM_API_KEY = '87B1E6B2C33AA7850637787CF4BAC545'
+STEAM_API_KEY = 'super secret steam key'
 
 # Authentification for OpenID
 AUTHENTICATION_BACKENDS = (
