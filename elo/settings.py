ba755c13b777e1c66b3baf30fc51aa946a6dce15
David Reeve <nerdboy6@gmail.com>
2015-02-20 12:32:07 -0500

    Changed arrow detection logic
    Positions are now checked after players are switched. Also, trying to
    get the last activity on a ladder without activity returns "Never"

--- elo/settings.py
+++ elo/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'super secret key'
+SECRET_KEY = 'e54t54355s4di68@bdbajegui458&&5d4s/b38*54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
 
     'ladder',
     'elo',
+    'usercontrol',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -90,7 +91,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/gordon/public/laddr.org/elo/static/'
+#STATIC_ROOT = '/home/gordon/public/laddr.org/elo/static/'
 
 # Steam API key
 STEAM_API_KEY = '87B1E6B2C33AA7850637787CF4BAC545'
@@ -123,6 +124,6 @@ OPENID_USE_AS_ADMIN_LOGIN = True
 AUTH_PROFILE_MODULE = 'elo.UserProfile'
 
 # To allow sub-domain cross-site authenticating
-CSRF_COOKIE_DOMAIN = '.laddr.org'
+CSRF_COOKIE_DOMAIN = '127.0.0.1'
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
