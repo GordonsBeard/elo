123ec95650d53ae2f42eac8d35e685b26b8073e5
David Reeve <nerdboy6@gmail.com>
2015-02-26 13:35:56 -0500

    Exceptions now use std __init__ and __str__

--- ladder/exceptions.py
+++ ladder/exceptions.py
@@ -1,26 +1,16 @@
 
 class ChallengeValidationError( Exception ) :
-    def __init__( self, error ) :
-       self.value = error
-
-    def __str__( self ) :
-        return repr( self.value )
+    pass
 
 class ChallengeeIsChallenger( ChallengeValidationError ) :
     pass
 
 class ChallengeeOutOfRange( ChallengeValidationError ) :
-    def __init__( self, error, rankdiff ) :
-        self.value    = error
-        self.rankdiff = rankdiff
+    pass
 
 class ParticipantBusy( ChallengeValidationError ) :
-    def __init__( self, error, user ) :
-        self.value = error
-        self.user  = user
+    pass
 
 class PlayerNotRanked( ChallengeValidationError ) :
-    def __init__( self, error, ladder ) :
-        self.value  = error
-        self.ladder = ladder
+    pass
 
