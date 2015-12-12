77f901997d34aaafffbd58d0e1d09a276f4585ea
Gordon <thisisgordonsemail@gmail.com>
2015-12-12 13:05:09 -0800

    Added SteamProfile inline for admin panels

---
+++ elo/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from elo.models import UserProfile
+
+class SteamProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "profiles"
+    readonly_fields = ("handle", "steamid", "url", "avatar", "avatarM", "avatarL", "primarygroup", "realname", )
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (SteamProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
