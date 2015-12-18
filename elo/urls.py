66401b74a239bba19b2c8181e02322afd260d66c
Gordon <thisisgordonsemail@gmail.com>
2015-12-17 19:40:05 -0800

    Fixed some POST urls

--- elo/urls.py
+++ elo/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^openid/complete/$',      elo.views.login),
     url(r'^openid/',                include('django_openid_auth.urls')),
     url(r'^logout/$',               elo.views.logout_view),
+    url(r'^create/$',               ladder.views.create_ladder),
 ]
